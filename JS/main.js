96a2d47726f7c17dadd93d8c986dca9ba7712db7
Kitsune Nishizono <rrangel@hub-camp.mx>
2018-02-24 10:19:43 -0600

    se agregan eventos y propiedades a un botón

---
+++ JS/main.js
@@ -0,0 +1,13 @@
+var button = document.getElementById("my_button");
+//Guía de sintaxis
+//object.addEventListener("event_name", function(){
+//});
+button.addEventListener("click", function(){
+	alert("Click");
+});
+/*object.addEventListener("event_name", onClick);
+object.removedEventListener("event_name", otherClick);
+function onClick(){
+}
+function otherClick {
+}*/
